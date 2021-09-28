7b0b8c1c7549c0eba8718097d85192c66de26446
Yair-Gonzalez <91285048+Yair-Gonzalez@users.noreply.github.com>
2021-09-28 16:20:24 -0700

    added first homework

---
+++ first-homework.py
@@ -0,0 +1,15 @@
+
+
+
+#define a main function
+def main():
+
+	#print name and pronouns
+	print("Yair Gonzalez, He/Him/His")
+
+
+
+	#if in the main subroutine, run main() function
+	if__name__=="__main__"
+	main()
+
